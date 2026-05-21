f288a35a625892d7156a738685ac72bc74bab411
Tim Tan <timtan@timdembp-2.mynetworksettings.com>
2026-05-21 09:11:05 -0400

    complete input and output practice

--- practice_input.py
+++ practice_input.py
@@ -3,14 +3,14 @@ A little assignment to practice receiving text input from the user in Python pro
 Do not run this file... run main.py instead.
 """
 
-
 def get_favorite_vegetable():
     """
     Asks the user to enter their favorite vegetable
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite vegetable.
     """
-    # write your code here.
+    favorite_vegetable = input("")
+    print(f"Interesting! I also love {favorite_vegetable}!")
 
 
 def get_favorite_number():
@@ -19,7 +19,8 @@ def get_favorite_number():
     and then prints out, "Interesting! I also love X!",
     where X is replaced with the user's favorite number.
     """
-    # write your code here.
+    favorite_number =input("")
+    print(f"Interesting! I also love {favorite_number}!")
 
 
 def get_name_and_zodiac_sign():
@@ -29,7 +30,10 @@ def get_name_and_zodiac_sign():
     Then print out, "Interesting! My name is also X, and I'm also a Y!",
     where X and Y are replaced by the user's name and zodiac sign, respectively.
     """
-    # write your code here.
+    name = input("")
+    zodiac_sign = input("")
+    print(f"Interesting! My name is also {name}, and I'm also a {zodiac_sign}!")
+
 
 
 def get_name_and_age():
@@ -39,4 +43,6 @@ def get_name_and_age():
     Then print out, "Interesting! My name is also X, and I'm also Y years old!",
     where X and Y are replaced by the user's name and age, respectively.
     """
-    # write your code here.
+    name = input("")
+    age = input("")
+    print(f"Interesting! My name is also {name}, and I'm also {age} years old!")
